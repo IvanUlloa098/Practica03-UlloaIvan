99a896d29866207a914aa9a9abd5d0a7794d5367
IvanUlloa098 <iulloa@est.ups.edu.ec>
2020-05-10 11:30:29 -0500

    Calculadora usando Javascript

---
+++ calculadora.js
@@ -0,0 +1,80 @@
+function figual() {
+    x=document.getElementById('area_cal').value;
+    r=eval(x);
+    document.getElementById('area_cal').value=r;
+}
+
+function fclear() {
+    document.getElementById('area_cal').value='';
+}
+
+//Botones
+function fbtn1() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'1';
+}
+
+function fbtn2() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'2';
+}
+
+function fbtn3() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'3';
+}
+
+function fbtn4() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'4';
+}
+
+function fbtn5() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'5';
+}
+
+function fbtn6() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'6';
+}
+
+function fbtn7() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'7';
+}
+
+function fbtn8() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'8';
+}
+
+function fbtn9() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'9';
+}
+
+function fbtn0() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'0';
+}
+
+function fbtnmas() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'+';
+}
+
+function fbtnmen() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'-';
+}
+
+function fbtnmul() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'*';
+}
+
+function fbtndiv() {
+    x=document.getElementById('area_cal').value;
+    document.getElementById('area_cal').value=x+'/';
+}
